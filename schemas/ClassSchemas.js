2d7d4d53456e8483114b697c98e4a65e453a5e4d
Pujarani06052001 <ranipooja21@navgurukul.org>
2024-10-10 23:47:13 +0530

    Add class with description and time

--- schemas/ClassSchemas.js
+++ schemas/ClassSchemas.js
@@ -1,24 +1,29 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
-
-// Class Schema
 const ClassSchema = new Schema({
   name: {
     type: String,
     required: true,
   },
+  description: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: String, 
+    required: true,
+  },
   students: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'User', // references User schema (students)
+      ref: 'User', 
     },
   ],
   teacher: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User', // references User schema (teacher)
+    ref: 'User', 
   },
 });
 
-// Ensure you are exporting the Class model as default
 const Class = mongoose.model('Class', ClassSchema);
-export default Class;  // Change this line to export default
+export default Class; 
